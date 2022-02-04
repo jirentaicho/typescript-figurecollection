d573e265e7555e44b2b8696050664b54b0e47366
jiren <yujienjoyfever@yahoo.co.jp>
2022-02-04 16:18:25 +0900

    PageLogic#initをDomCreatorを利用するように修正

--- src/app/PageLogic.ts
+++ src/app/PageLogic.ts
@@ -1,5 +1,6 @@
 import Animate from "../core/anim/Animate";
 import Dom from "../core/dom/Dom";
+import DomCreator from "../core/dom/DomCreator";
 import DomConst from "../type/DomConst";
 
 export default class PageLogic{
@@ -7,6 +8,12 @@ export default class PageLogic{
     private constructor(){}
 
     private static loaded = false;
+
+    private static readonly ITEM_AREA_STYLE = "w-full overflow-y-scroll bg-slate-300 p-5 block flex flex-wrap";
+
+    private static readonly SHELF_AREA_STYLE = "w-full overflow-y-scroll bg-slate-300 p-5 block flex flex-wrap";
+
+    private static readonly OVER_RAY_STYLE = "fixed hidden left-0 top-0 flex justify-center items-center w-full min-h-screen z-20 bg-black bg-opacity-75";
 
     public static init(): void{
         //初期処理が完了している場合は処理しない
@@ -16,34 +23,36 @@ export default class PageLogic{
         PageLogic.loaded = true;
         
         // ITEMエリアの作成
-        const element = <HTMLDivElement>document.createElement('div');
-        element.setAttribute('id', Dom.ITEM_AREA_STR);
-        element.setAttribute('class', "w-full overflow-y-scroll bg-slate-300 p-5 block flex flex-wrap");
-        element.innerHTML="私のコレクション";
-        document.body.appendChild(element);
+        const itemarea = new DomCreator('div')
+                            .idName(DomConst.ITEM_AREA_ID)
+                            .className(PageLogic.ITEM_AREA_STYLE)
+                            .build();
+        itemarea.innerHTML="私のコレクション";
+        document.body.appendChild(itemarea);
 
         // 棚エリアの作成
-        const shelfarea = <HTMLDivElement>document.createElement('div');
-        shelfarea.setAttribute('id', Dom.SHELF_AREA);
-        shelfarea.setAttribute('class', "w-full overflow-y-scroll bg-slate-300 p-5 block flex flex-wrap");
+        const shelfarea = new DomCreator('div')
+                            .idName(DomConst.SHELF_AREA_ID)
+                            .className(PageLogic.SHELF_AREA_STYLE)
+                            .build();
         document.body.appendChild(shelfarea);
 
         // オーバーレイエリアの作成
-        const overrayarea = <HTMLDivElement>document.createElement('div');
-        overrayarea.setAttribute('id', Dom.OVER_RAY_AREA);
-        overrayarea.setAttribute('class', "fixed hidden left-0 top-0 flex justify-center items-center w-full min-h-screen z-20 bg-black bg-opacity-75");
+        const overrayarea = new DomCreator('div')
+                            .idName(DomConst.OVERRAY_AREA_ID)
+                            .className(PageLogic.OVER_RAY_STYLE)
+                            .build();
         // オーバーレイを閉じる時の動作になります。
         overrayarea.addEventListener('click', function(){
             Animate.PlayWithId(DomConst.CURRENT_IMAGE_ID, {
-                scale: [0.6, 1, 0.1],
-                duration: 800,
+                scale: [0.6, 0.7, 0.1],
+                duration: 400,
                 easing: 'easeInOutSine',
                 complete: ()=> {
                     this.classList.add('hidden');
                 }
             });
         });
-        
         document.body.appendChild(overrayarea);
     }
 
--- src/type/DomConst.ts
+++ src/type/DomConst.ts
@@ -6,4 +6,11 @@ export default class DomConst{
     public static readonly CURRENT_IMAGE_ID = 'cuurrent_img';
 
     public static readonly HIDDEN = 'hidden';
+
+    public static readonly ITEM_AREA_ID = 'item_area';
+
+    public static readonly SHELF_AREA_ID = 'shelf_area';
+
+    public static readonly OVERRAY_AREA_ID = 'over_ray';
+
 }
